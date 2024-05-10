7fa6205577422db953df43394fcc4ee66c8e21c9
abrordc <moh.abrorilhuda@gmail.com>
2024-05-10 15:40:01 +0700

    new repostry

---
+++ JS-pemogramanweb/variable.js
@@ -0,0 +1,19 @@
+/*
+penulis variabel di js itu ada tiga namun
+yang di recomendasikan ada dua 
+yaitu let and const
+*/
+
+// penjelasan kenapa var sudah tidak di recomendasikan
+/*
+var tidak lagi disarankan untuk mendeklarasikan
+variabel di JavaScript modern karena perilaku
+hoisting dan function scope. Variabel yang
+dideklarasikan dengan var memiliki cakupan
+fungsi atau cakupan global, dan terlihat
+melalui blok. Var mengabaikan blok kode,
+sehingga variabel global akan tetap ada. 
+*/
+const name = "abror";
+let name = "abror";
+var name = " abror";
---
+++ README.md
@@ -0,0 +1,20 @@
+# LAPRAK PEMOGRAMAN WEB 
+---
+
+### DOSEN PENGAMPU : 
+### ABD WAHAB SYAHRONI, S.Kom, M.Kom
+
+materi praktikum:
+
+* [html](https://github.com/DemtimCod/Modul-html/blob/main/dicoding-modul-html.md)
+* [css](https://github.com/DemtimCod/Modul-html/blob/main/Img-modul-html.md)
+* [java script](https://github.com/DemtimCod/Modul-html/blob/main/Formatting-modul-html.md)
+* [php](https://github.com/DemtimCod/Modul-html/blob/main/tabel-modul-html.md) 
+
+
+
+
+<code>author{
+  username: "abrordc", 
+  semester: 4
+}<code/>
