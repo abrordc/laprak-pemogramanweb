02bbe2d66316dc1fb83df4064f0f0a777fe07723
abrordc <moh.abrorilhuda@gmail.com>
2024-05-14 11:50:44 +0700

    14 maret

---
+++ PHP-pemogramanweb/variable.php
@@ -0,0 +1,6 @@
+<?php
+
+// penulisan variabel di php bisa menggunakan menggunakan symbol $ dolar lalu nama variable dan nama tidak boleh di awali angka dan symbol contoh 
+
+$nama = "nama";
+const nama = "nama";
